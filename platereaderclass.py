efd834363824f225611b72f5e7245afc27e0c1fc
Luka Geyrhofer <lukas.geyrhofer@gmx.at>
2018-04-15 11:43:53 +0300

    reimplemented estimation with new dataclass

--- platereaderclass.py
+++ platereaderclass.py
@@ -22,6 +22,10 @@ class PlateReaderData(object):
         self.__sheetnames = list()
         self.__designassignment = list()
         
+        self.__rescale = kwargs.get("DataRescale",False)
+        self.__logdata = kwargs.get("DataLogscale",False)
+        self.__logmin  = kwargs.get("DataLogscaleMin",-20)
+        
         self.__read_coordinates = { 'x0':kwargs.get("xstart",2),
                                     'y0':kwargs.get("ystart",2),
                                     'xwidth':kwargs.get("xwidth",12),
@@ -47,7 +51,10 @@ class PlateReaderData(object):
                 self.__sheetnames.append(sheet.title)
                 self.__filenames.append(fn)
                 try:
-                    self.__designassignment.append(kwargs.get("designassignment",[])[i])
+                    design = kwargs.get("designassignment",[])[i]
+                    if not 0 <= design < len(self.__designdata):
+                        raise KeyError
+                    self.__designassignment.append(design)
                 except:
                     self.__designassignment.append(0)
                 i+=1
@@ -66,6 +73,21 @@ class PlateReaderData(object):
         return string
     
     
+    def rgb(self,color):
+        r = int(color[0:2],16)
+        g = int(color[2:4],16)
+        b = int(color[4:6],16)
+        return np.array([r/255.,g/255.,b/255.])
+
+
+    def avg(self,values,geom = True):
+        if len(values) > 0:
+            if geom:    return np.power(np.product(values),1./len(values))
+            else:       return np.mean(values)
+        else:
+            raise ValueError
+    
+    
     def read_sheet(self,sheet,x0 = None, y0 = None, width = None, height = None):
         if x0 is None:      x0 = self.__read_coordinates['x0']
         if y0 is None:      y0 = self.__read_coordinates['y0']
@@ -73,6 +95,7 @@ class PlateReaderData(object):
         if height is None:  height = self.__read_coordinates['yheight']
         
         return np.array([[sheet['{:s}{:d}'.format(self.column_string(i),j)].value for i in range(x0,x0+width)] for j in range(y0,y0+height)])
+
 
     def read_initial_conditions(self,data,sheetname,xab = 4, yab = 14, xcells = 4, ycells = 3, width = 12, height = 8):
         if sheetname in data.sheetnames:
@@ -96,16 +119,18 @@ class PlateReaderData(object):
                     r = True
         return r
     
-    def rescale(g,logscale=False,logmin=-20):
+    
+    def rescale(self,g):
         
-        if logscale:
+        if self.__logscale:
             r = np.log(g)
-            r[r<logmin] = 0
+            r[r<self.__logmin] = self.__logmin
         else:
             r = g[:,:]
             
         r = (r - np.min(r))/(np.max(r) - np.min(r))
         return r
+    
     
     def extract_figure_file_parameters(self,kwargs):
         self.figureparameters = {   'colors':   ['3465a4','ffffff','2e3436','eeeeec'],
@@ -115,15 +140,16 @@ class PlateReaderData(object):
 
 
 
+
     def write_PNG(self,dataid,outfilename = None):
         if 0 <= dataid < len(self.__data):
             if outfilename is None:
                 outfilename = self.__sheetnames[dataid] + '.png'
                 
-            cFull   = rgb(self.figureparameters['colors'][0])
-            cEmpty  = rgb(self.figureparameters['colors'][1])
-            cBorder = rgb(self.figureparameters['colors'][2])
-            cBack   = rgb(self.figureparameters['colors'][3])
+            cFull   = self.rgb(self.figureparameters['colors'][0])
+            cEmpty  = self.rgb(self.figureparameters['colors'][1])
+            cBorder = self.rgb(self.figureparameters['colors'][2])
+            cBack   = self.rgb(self.figureparameters['colors'][3])
 
             CairoImage = cairo.ImageSurface(cairo.FORMAT_ARGB32,data.shape[1] * self.figureparameters['wellsize'],data.shape[0] * self.figureparameters['wellsize'])
             context    = cairo.Context(CairoImage)
@@ -161,6 +187,17 @@ class PlateReaderData(object):
         else:
             return self.__designdata[designid]
     
+    
+    def get_designassignment(self,dataid = None):
+        if dataid is None:
+            return self.__designassignment
+        else:
+            if 0 <= dataid < len(self.__designassignment):
+                return self.__designassignment[dataid]
+            else:
+                raise KeyError
+    
+    
     def count_design(self):
         assert len(self.__designdata) == len(self.__designtitle)
         return len(self.__designdata)
@@ -172,28 +209,32 @@ class PlateReaderData(object):
 
     def compute_growth_nogrowth_transition(self,dataid,threshold,design = 0,geom = True):
         r=list()
-        print "hi",dataid,threshold,design
         for i,j in itertools.product(np.arange(np.shape(self.__data[dataid])[0]),np.arange(np.shape(self.__data[dataid])[1])):
-            print i,j
             try:
                 if (self.__data[dataid][i,j] - threshold) * (self.__data[dataid][i+1,j] - threshold) < 0:
-                    r.append(np.array([avg([self.__designdata[design][0][i,j],self.__designdata[design][0][i+1,j]],geom),avg([self.__designdata[design][1][i,j],self.__designdata[design][1][i+1,j]],geom)]))
+                    r.append(np.array([self.avg([self.__designdata[design][0][i,j],self.__designdata[design][0][i+1,j]],geom),self.avg([self.__designdata[design][1][i,j],self.__designdata[design][1][i+1,j]],geom)]))
             except:
                 pass
             try:
                 if (self.__data[dataid][i,j] - threshold) * (self.__data[dataid][i,j+1] - threshold) < 0:
-                    r.append(np.array([avg([self.__designdata[design][0][i,j],self.__designdata[design][0][i,j+1]],geom),avg([self.__designdata[design][1][i,j],self.__designdata[design][1][i,j+1]],geom)]))
+                    r.append(np.array([self.avg([self.__designdata[design][0][i,j],self.__designdata[design][0][i,j+1]],geom),self.avg([self.__designdata[design][1][i,j],self.__designdata[design][1][i,j+1]],geom)]))
             except:
                 pass
-            print r
-            if len(r) > 0:
-                return np.vstack(r)
-            else:
-                return None
+        if len(r) > 0:
+            return np.vstack(r)
+        else:
+            return None
+
 
     def transitions(self,threshold):
         return [(self.__filenames[i],self.__sheetnames[i],self.compute_growth_nogrowth_transition(i,threshold,self.__designassignment[i])) for i in range(len(self.__data))]
 
+
     def __iter__(self):
         for fn,title,platedata in zip(self.__filenames,self.__sheetnames,self.__data):
-            yield fn,title,platedata
+            yield fn,title,self.rescale(platedata)
+    
+    
+    def __len__(self):
+        return len(self.__data)
+    
