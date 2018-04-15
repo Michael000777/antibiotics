ec79dbde7023bfbcffbf156793d95a2a06375bd8
Luka Geyrhofer <lukas.geyrhofer@gmx.at>
2018-04-15 11:45:26 +0300

    again

---
+++ estimate_Tau_ssMIC.py
@@ -0,0 +1,111 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function
+
+import numpy as np
+import argparse
+import openpyxl
+import cairo
+import sys,math
+import uncertainties
+import itertools
+
+from uncertainties.umath import exp as uexp
+from skimage import measure
+from scipy.optimize import curve_fit
+
+import platereaderclass as prc
+
+# *****************************************************************
+# ** helper routines
+# *****************************************************************
+
+def LMSQ(x,y):
+    n   = len(x)
+    sx  = np.sum(x)
+    sy  = np.sum(y)
+    sxx = np.dot(x,x)
+    sxy = np.dot(x,y)
+    syy = np.dot(y,y)
+    
+    denom  = (n*sxx-sx*sx)
+    b      = (n*sxy - sx*sy)/denom
+    a      = (sy-b*sx)/n
+    estim  = np.array([a,b],dtype=np.float)
+
+    sigma2 = syy + n*a*a + b*b*sxx + 2*a*b*sx - 2*a*sy - 2*b*sxy
+    cov    = sigma2 / denom * np.array([[sxx,-sx],[-sx,n]],dtype=np.float)
+
+    return estim,cov
+
+
+# *****************************************************************
+# ** process data
+# *****************************************************************
+
+def estimate_Tau_sMIC_linearFit(initialconditions,ABlambda = 1):
+    # theory predicts: N > 1 + lambda/tau LOG(B/sMIC)
+    Nm1 = initialconditions[:,1] - 1
+    lB  = np.log(initialconditions[:,0])
+    
+    fit,cov = LMSQ(lB,Nm1)
+    ret     = uncertainties.correlated_values(fit,cov)
+    
+    u_tau   = ABlambda/ret[1]
+    u_sMIC  = uexp(-ret[0]/ret[1])
+    
+    tau     = np.array([uncertainties.nominal_value(u_tau), uncertainties.std_dev(u_tau)])
+    sMIC    = np.array([uncertainties.nominal_value(u_sMIC),uncertainties.std_dev(u_sMIC)])
+    
+    return tau,sMIC
+
+
+def estimate_Tau_sMIC_singleParameter(initialconditions,ABlambda = 1):
+    mincelldens = np.min(initialconditions[:,1])
+    smic_list   = [m[0] for m in initialconditions if m[1] == mincelldens]
+    smic        = np.power(np.prod(smic_list),1./len(smic_list))
+    
+    lBM  = np.log(initialconditions[:,0]/smic)
+    n    = initialconditions[:,1]
+    
+    tau1 = np.sum(lBM*lBM/n)/ ((np.sum(lBM) - np.sum(lBM/n)) * ABlambda)
+    tau2 = np.dot(n-1,lBM)/(np.dot(n-1,n-1) * ABlambda)
+    return tau1,tau2,smic
+
+
+# *****************************************************************
+# ** main
+# *****************************************************************
+
+def main():
+    ignoresheets = ["Plate design"]
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--infiles",          nargs = "*")
+    parser.add_argument("-t", "--growthThreshold",  type = float, default = 0.1)
+    parser.add_argument("-D", "--designassignment", type = int, nargs = "*", default = [])
+    
+    parser.add_argument("-P", "--noImages",         default = False, action = "store_true")
+    parser.add_argument("-T", "--noThresholdFiles", default = False, action = "store_true")
+    parser.add_argument("-F", "--noDataFiles",      default = False, action = "store_true")
+    
+    parser.add_argument("-L", "--AB_lambda",        type = float, default = 1)
+    parser.add_argument("-l", "--AB_lambdaStdDev" , type = float, default = 0)
+    args = parser.parse_args()
+    
+    # use uncertainties package to compute error propagation
+    ABlambda = uncertainties.ufloat(args.AB_lambda,args.AB_lambdaStdDev)
+    
+    data = prc.PlateReaderData(**vars(args))
+    
+    for fn,title,transitions in data.transitions(threshold = args.growthThreshold):
+        tau1,smic1 = estimate_Tau_sMIC_linearFit(transitions)
+        tau2,tau3,smic2 = estimate_Tau_sMIC_singleParameter(transitions)
+        
+        print("{:40s} {:14.6e} {:14.6e} {:14.6e} {:14.6e} {:14.6e} {:14.6e} {:14.6e}".format(title,tau1[0],tau1[1],smic1[0],smic1[1],tau2,tau3,smic2))
+    
+
+if __name__ == "__main__":
+    main()
+
