9864bde4ae717869f220add95e274e6510c84207
Luka Geyrhofer <lukas.geyrhofer@gmx.at>
2018-06-14 14:17:48 +0300

    more structure to output

--- transform_data.py
+++ transform_data.py
@@ -10,36 +10,27 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("-i","--infiles",nargs="*",default=[])
     parser.add_argument("-t","--timeinterval",type=float,default=10)
+    parser.add_argument("-v","--verbose",action="store_true",default=False)
     args = parser.parse_args()
 
-    alldata = dict()
+    alldata = list()
+    alllabels = list()
 
     for fn in args.infiles:
-        print "# " + fn
+        if args.verbose:
+            print "# " + fn
         data = np.genfromtxt(fn)
-        
-        #print data
-        #print data[:,0]
-        #exit(1)
         
         labels = data[:,0]
         n = data[:,1:].T
         time = np.arange(len(n[:,0])) * args.timeinterval
         
-        #print labels
-        #print t
-        #print n
-        
         for i,label in enumerate(labels):
-            if not label in alldata.keys():
-                alldata[label] = n[:,i]                
-
-    print "# " + " ".join(["{}".format(key) for key in alldata.keys()])
+            alllabels.append(label)
+            alldata.append(n[:,i])
+    print "# " + " ".join(["{}".format(label) for label in alllabels])
     for i,t in enumerate(time):
-        print t,
-        for key in alldata.keys():
-            print alldata[key][i],
-        print
+        print t," ".join(["{}".format(traj[i]) for traj in alldata])
 
 
 
