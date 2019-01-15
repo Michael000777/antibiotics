ea314f7af2b6fd6a87b0ffbb2beec509f8e12fd7
Luka Geyrhofer <lukas.geyrhofer@gmx.at>
2019-01-15 18:15:13 +0200

    can now use arbitrary kernels from cmdline

--- plates_GPR.py
+++ plates_GPR.py
@@ -10,12 +10,43 @@ import sklearn.gaussian_process as sklgp
 
 import platereaderclass as prc
 
+
+def generate_kernel(argumets):
+    available_kernels = ['CONST', 'WHITE', 'MATERN', 'RBF', 'EXPSINESQUARED', 'DOTPRODUCT', 'RATIONALQUADRATIC']
+    def add_kernel(kernel,newkernel):
+        if not kernel is None:
+            if   k == 'CONST':              kernel += sklgp.kernels.ConstantKernel()
+            elif k == 'WHITE':              kernel += sklgp.kernels.WhiteKernel()
+            elif k == 'MATERN':             kernel += sklgp.kernels.Matern()
+            elif k == 'RBF':                kernel += sklgp.kernels.RBF()
+            elif k == 'EXPSINESQUARED':     kernel += sklgp.kernels.ExpSineSquared()
+            elif k == 'DOTPRODUCT':         kernel += sklgp.kernels.DotProduct()
+            elif k == 'RATIONALQUADRATIC':  kernel += sklgp.kernels.RationalQuadratic()
+        else:
+            if   k == 'CONST':              kernel  = sklgp.kernels.ConstantKernel()
+            elif k == 'WHITE':              kernel  = sklgp.kernels.WhiteKernel()
+            elif k == 'MATERN':             kernel  = sklgp.kernels.Matern()
+            elif k == 'RBF':                kernel  = sklgp.kernels.RBF()
+            elif k == 'EXPSINESQUARED':     kernel  = sklgp.kernels.ExpSineSquared()
+            elif k == 'DOTPRODUCT':         kernel  = sklgp.kernels.DotProduct()
+            elif k == 'RATIONALQUADRATIC':  kernel  = sklgp.kernels.RationalQuadratic()
+        return kernel
+    kernel = None
+    klist = [ku.upper() for ku in args.kernels if ku.upper() in available_kernels]
+    for k in klist:
+        kernel = add_kernel(kernel,k)
+    if not kernel is None:
+        return kernel
+    else:
+        raise ValueError('could not define kernel')
+
 parser = argparse.ArgumentParser()
 parser.add_argument("-i","--infiles",nargs="*",default=[])
 parser.add_argument("-D","--designassignment",nargs="*",default=None)
 parser.add_argument("-n","--outputgrid",default=50,type=int)
 parser.add_argument("-E","--error_estimates",default=False,action="store_true")
 parser.add_argument("-o","--outfileprefix",default="GPR",type=str)
+parser.add_argument("-K","--kernels",nargs="*", default = "const white matern")
 args = parser.parse_args()
 
 data = prc.PlateReaderData(**vars(args))
@@ -30,8 +61,10 @@ for dataID in range(len(data)):
     platedata         = np.array([data[dataID].flatten()]).T
     if args.error_estimates:
         errorestimate = data.get_noise_estimates(dataID = dataID).flatten()
+        
+            
 
-    kernel = sklgp.kernels.RBF(1,(1e-5,1e5))
+    kernel = generate_kernel(args.kernels)
     if args.error_estimates:    gp = sklgp.GaussianProcessRegressor(kernel = kernel, n_restarts_optimizer = 10, alpha = errorestimate)
     else:                       gp = sklgp.GaussianProcessRegressor(kernel = kernel, n_restarts_optimizer = 10)
 
