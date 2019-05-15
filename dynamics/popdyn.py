b5ee7d62427cc3d8b2b3f0329bbbd0bd48baa9f2
Lukas Geyrhofer <lukas.geyrhofer@gmx.at>
2019-05-15 21:22:59 +0300

    dynamics

--- dynamics/popdyn.py
+++ dynamics/popdyn.py
@@ -12,8 +12,11 @@ class TimeIntegratorDynamics(object):
     def __init__(self,**kwargs):
         
         self.__params = dict(kwargs)
-        print(self.__params)
+        for key in self.__params:
+            if isinstance(self.__params[key],(list,tuple)):
+                self.__params[key] = np.array(self.__params[key])
 
+        self.__verbose = kwargs.get('verbose',False)
         
         na  = len(self.__params.get('PD_growthrate'))
         ny  = len(self.__params.get('PD_yield'))
@@ -25,52 +28,65 @@ class TimeIntegratorDynamics(object):
         
         self.__numstrains = na
         
-        init = np.concatenate([ np.zeros(1),
+        self.__init = np.concatenate([
+                                np.zeros(1),
                                 np.array(self.__params.get('PD_initsize'),dtype=np.float),
                                 np.array([self.__params.get('PD_initsubstr')],dtype=np.float),
                                 np.zeros(self.__numstrains),
                                 np.zeros(self.__numstrains),
                                 np.zeros(1),
-                                np.array([self.__params.get('AB_initconc')]) ])
+                                np.array([self.__params.get('AB_initconc')])
+                            ])
         
-        headers = np.concatenate([  ['time'],
+        self.__headers = np.concatenate([
+                                    ['time'],
                                     np.array(['N{:d}'.format(i) for i in range(self.__numstrains)]),
                                     ['S'],
                                     np.array(['Ein{:d}'.format(i) for i in range(self.__numstrains)]),
                                     np.array(['Bin{:d}'.format(i) for i in range(self.__numstrains)]),
                                     ['Eout'],
-                                    ['Bout']    ])
+                                    ['Bout']
+                                ])
         
-        self.__data = pd.DataFrame([init],columns = headers)
+        self.__data = pd.DataFrame([self.__init],columns = headers)
 
 
-    def RungeKutta4(self,func, xx ,time = 0, step = 1e-3):
+    def RungeKutta4(self, func, xx ,time = 0, step = 1e-3):
         # 4th order Runge-Kutta integration scheme
-        k1 = step * func(time,              xx )
-        k2 = step * func(time + 0.5 * step, xx + 0.5 * k1)
-        k3 = step * func(time + 0.5 * step, xx + 0.5 * k2)
-        k4 = step * func(time + step,       xx + k3 )
-        return xx + (k1 + 2 * k2 + 2 * k3 + k4)/6.
+        k1 = step * func(xx,            time)
+        k2 = step * func(xx + 0.5 * k1, time + 0.5 * step)
+        k3 = step * func(xx + 0.5 * k2, time + 0.5 * step)
+        k4 = step * func(xx + k3,       time + step)
+        return xx + (k1 + 2. * k2 + 2. * k3 + k4)/6.
 
 
-    def SingleStep(self):
-        x    = np.array(self.__data.tail(1)).flatten()
-        xnew = self.RungeKutta4(self.dynamics, x)
-        self.__data.append(xnew)
+    def Step(self):
+        x = np.array(self.__data.tail(1)).flatten()
+        for s in range(self.__params.get('ALG_outputstep',20)):
+            xnew = self.RungeKutta4(self.dynamics, x)
+            x    = xnew[:]
+        if self.__verbose:
+            print(x)
+        self.__data.append(pd.DataFrame(x))
     
     
-    def dynamics(self, state, time):
-        N    = self.__data.loc['N0':'N{:d}'.format(self.__numstrains)].copy()
-        S    = self.__data.loc['S']
-        Ein  = self.__data.loc['Ein0':'Ein{:d}'.format(self.__numstrains)].copy()
-        Bin  = self.__data.loc['Bin0':'Bin{:d}'.format(self.__numstrains)].copy()
-        Eout = self.__data.loc['Eout']
-        Bout = self.__data.loc['Bout']
+    def growthrateEff(self,Bin):
+        bink = np.power(Bin,self.__params.get('AB_kappa',2))
+        return self.__params.get('PD_growthrate') * (1+self.__params.get('AB_gamma',2)) * bink/(bink + self.__params.get('AB_gamma',2))
+    
+    
+    def dynamics(self, x, time):
+        N    = x[1:1+self.__numstrains]
+        S    = x[1+self.__numstrains]
+        Ein  = x[2+self.__numstrains:2+2*self.__numstrains]
+        Bin  = x[2+2*self.__numstrains:2+3*self.__numstrains]
+        Eout = x[2+3*self.__numstrains]
+        Bout = x[3+3*self.__numstrains]
         
         return np.concatenate([
                         [self.__params.get('ALG_integratorstep',1e-3)],
                         np.array(self.growthrateEff(Bin) * N),
-                        [-np.sum(self.growthrateEff(np.zeros(self.__numstrains))/self.__params.get('yield') * N)],
+                        [-np.sum(self.growthrateEff(np.zeros(self.__numstrains))/self.__params.get('PD_yield') * N)],
                         np.array(self.__params.get('PD_rho') + self.__params.get('PD_sigma') * (Ein - Eout)),
                         np.array(self.__params.get('AB_epsilon') * Ein/(Ein + self.__params.get('AB_michaelismenten')) * Bin + self.__params.get('AB_sigma') * (Bin - Bout)),
                         [np.sum(self.__params.get('PD_sigma') * N * self.__params.get('PD_volumeseparation') * (Eout - Ein))],
@@ -82,7 +98,7 @@ class TimeIntegratorDynamics(object):
 
     def run(self,steps = 100):
         for s in range(steps):
-            self.SingleStep()
+            self.Step()
     
     def save_data(self):
         self.__data.to_csv(self.__params.get('ALG_outfilename','out'), sep=' ', index_label = '#')
@@ -106,13 +122,16 @@ def main():
     parser_PopDyn.add_argument("-y", "--PD_yield",            default = [1],   type = float, nargs = "*")
     parser_PopDyn.add_argument("-r", "--PD_rho",              default = [1],   type = float, nargs = "*")
     parser_PopDyn.add_argument("-p", "--PD_sigma",            default = [1],   type = float, nargs = "*")
-    parser_PopDyn.add_argument("-v", "--PD_volumeseparation", default = 1e-7,  type = float)
+    parser_PopDyn.add_argument("-V", "--PD_volumeseparation", default = 1e-7,  type = float)
     
     parser_alg = parser.add_argument_group(description = "==== Algorithm parameters ====")
     parser_alg.add_argument("-t", "--ALG_integratorstep", default = 1e-3,  type = float)
     parser_alg.add_argument("-T", "--ALG_runtime",        default = 24,    type = float)
     parser_alg.add_argument("-O", "--ALG_outputstep",     default = 100,   type = float)
-    parser_alg.add_argument("-o", "--ALG_outfilename",    default = "out", type = str)
+    
+    parser_io = parser.add_argument_group(description = "==== I/O parameters ====")
+    parser_io.add_argument("-o", "--outfilename",    default = "out", type = str)
+    parser_io.add_argument("-v", "--verbose",        default = False, action = 'store_true')
     
     args = parser.parse_args()
     
