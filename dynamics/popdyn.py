8165bf7f95965c899627fbafd8d9e20199b7b80f
Lukas Geyrhofer <lukas.geyrhofer@gmx.at>
2019-05-19 17:39:29 +0300

    running version

--- dynamics/popdyn.py
+++ dynamics/popdyn.py
@@ -29,7 +29,6 @@ class TimeIntegratorDynamics(object):
         self.__numstrains = na
         
         self.__init = np.concatenate([
-                                np.zeros(1),
                                 np.array(self.__params.get('PD_initsize'),dtype=np.float),
                                 np.array([self.__params.get('PD_initsubstr')],dtype=np.float),
                                 np.zeros(self.__numstrains),
@@ -39,7 +38,6 @@ class TimeIntegratorDynamics(object):
                             ])
         
         self.__headers = np.concatenate([
-                                    ['time'],
                                     np.array(['N{:d}'.format(i) for i in range(self.__numstrains)]),
                                     ['S'],
                                     np.array(['Ein{:d}'.format(i) for i in range(self.__numstrains)]),
@@ -49,6 +47,7 @@ class TimeIntegratorDynamics(object):
                                 ])
         
         self.__data = [self.__init]
+        self.__time = 0.
 
     def RungeKutta4(self, func, xx ,time = 0):
         # 4th order Runge-Kutta integration scheme
@@ -63,46 +62,57 @@ class TimeIntegratorDynamics(object):
         x = self.__data[-1]
         for s in range(self.__params.get('ALG_outputstep',20)):
             xnew = self.RungeKutta4(self.dynamics, x)
+            xnew[np.where(xnew < 0.)[0]]                     = 0.   # concentrations cannot be smaller than 1
+            xnew[np.where(xnew[:self.__numstrains] < 1.)[0]] = 0.   # populations are extinct if less than 1 individual
             x    = xnew[:]
-        if self.__verbose:
-            print(x)
-        self.__data = np.concatenate([self.__data,[x]], axis = 0)
+        self.__data  = np.concatenate([self.__data,[x]], axis = 0)
+        self.__time += self.__params['ALG_integratorstep'] * self.__params['ALG_outputstep']
+
+
     
-    
-    def growthrateEff(self,Bin):
-        bink = np.power(Bin,self.__params.get('AB_kappa',2))
-        return self.__params.get('PD_growthrate') * (1+self.__params.get('AB_gamma',2)) * bink/(bink + self.__params.get('AB_gamma',2))
+    def growthrateEff(self,Bin,substrate):
+        if substrate <= 0:
+            return np.zeros(self.__numstrains)
+        else:
+            bink = np.power(Bin,self.__params.get('AB_kappa',2))
+            return self.__params.get('PD_growthrate') * (1. - bink)/(1. + bink/self.__params.get('AB_gamma',2))
     
     
     def dynamics(self, x, time):
-        N    = x[1:1+self.__numstrains]
-        S    = x[1+self.__numstrains]
-        Ein  = x[2+self.__numstrains:2+2*self.__numstrains]
-        Bin  = x[2+2*self.__numstrains:2+3*self.__numstrains]
-        Eout = x[2+3*self.__numstrains]
-        Bout = x[3+3*self.__numstrains]
+        N    = x[0:self.__numstrains]
+        S    = x[self.__numstrains]
+        Ein  = x[1+self.__numstrains:1+2*self.__numstrains]
+        Bin  = x[1+2*self.__numstrains:1+3*self.__numstrains]
+        Eout = x[1+3*self.__numstrains]
+        Bout = x[2+3*self.__numstrains]
         
         return np.concatenate([
-                        [self.__params.get('ALG_integratorstep',1e-3)],
-                        np.array(self.growthrateEff(Bin) * N),
-                        [-np.sum(self.growthrateEff(np.zeros(self.__numstrains))/self.__params.get('PD_yield') * N)],
-                        np.array(self.__params.get('PD_rho') + self.__params.get('PD_sigma') * (Ein - Eout)),
+                        np.array(self.growthrateEff(Bin,S) * N),
+                        [-np.sum(self.growthrateEff(np.zeros(self.__numstrains),S)/self.__params.get('PD_yield') * N)],
+                        np.array(self.__params.get('PD_rho') - self.__params.get('PD_sigma') * (Ein - Eout)),
                         np.array(self.__params.get('AB_epsilon') * Ein/(Ein + self.__params.get('AB_michaelismenten')) * Bin + self.__params.get('AB_sigma') * (Bin - Bout)),
-                        [np.sum(self.__params.get('PD_sigma') * N * self.__params.get('PD_volumeseparation') * (Eout - Ein))],
-                        [-self.__params.get('AB_epsilon') * Eout/(Eout + self.__params.get('AB_michaelismenten')) + self.__params.get('AB_sigma') * self.__params.get('PD_volumeseparation') *  np.sum(N * (Bout - Bin))]
+                        [np.sum(self.__params.get('PD_sigma') * N * self.__params.get('PD_volumeseparation') * (Ein - Eout))],
+                        [-self.__params.get('AB_epsilon') * Eout/(Eout + self.__params.get('AB_michaelismenten')) - self.__params.get('AB_sigma') * self.__params.get('PD_volumeseparation') *  np.sum(N * (Bout - Bin))]
                     ])
-                                   
-        
 
 
     def run(self,steps = 100):
+        if self.__verbose and self.__time == 0: print(self.lastdata)
         for s in range(steps):
-            print(s)
             self.Step()
-    
+            if self.__verbose: print(self.lastdata)
+
+            
     def save_data(self):
-        d = pd.DataFrame(data = self.__data, columns = self.__headers)
-        d.to_csv(self.__params.get('ALG_outfilename','out'), sep=' ', index_label = '#')
+        t = np.array([np.arange(len(self.__data)) * self.__params.get('ALG_integratorstep',1e-3) * self.__params.get('ALG_outputstep')]).T
+        d = pd.DataFrame(data = np.concatenate([t, self.__data],axis=1), columns = np.concatenate([['time'],self.__headers]))
+        d.to_csv(self.__params.get('ALG_outfilename','out'), sep=' ', index_label = '#', float_format = '%.6e', index = False)
+
+
+    def __getattr__(self,key):
+        if key == "lastdata":
+            return '{:7.3f} '.format(self.__time) + ' '.join(['{:14.6e}'.format(v) for v in self.__data[-1]])
+
 
 
 def main():
