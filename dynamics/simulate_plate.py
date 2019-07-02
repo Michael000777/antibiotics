1fbf1fb6d5b775e663ffe63638ec03b8765623c2
Lukas Geyrhofer <lukas.geyrhofer@gmx.at>
2019-07-02 16:06:54 +0300

    transpose output, include threshold

--- dynamics/simulate_plate.py
+++ dynamics/simulate_plate.py
@@ -10,6 +10,16 @@ sys.path.append(sys.path[0] + '/..')
 import popdyn
 import platereaderclass as prc
 
+def computeThreshold(x):
+    x   = list(x.flatten())
+    sx  = np.sort(x)
+    lx  = len(x)
+    w   = np.arange(lx,dtype=np.float)/lx
+    m   = np.array([np.mean(sx[:k]) * w[k] if k > 0 else 0 for k in range(lx)])
+    mT  = np.mean(sx)
+    sB  = np.array([(mT * w[k] - m[k])**2/(w[k]*(1.-w[k])) if w[k]*(1.-w[k]) > 0 else 0 for k in range(lx)])
+    idx = np.argmax(sB)
+    return sx[idx]
 
 def main():
 
@@ -30,23 +40,24 @@ def main():
     bsize = 12
     nsize =  8
     
-    plate = np.zeros((bsize,nsize))
+    plate = np.zeros((nsize,bsize))
     
     for ndilution in np.arange(nsize):
         N0 = 2.4e1 * 4**ndilution
         sys.argv[N0index] = str(N0)
 
         for bdilution in np.arange(bsize):
-            B0 = 1e-2 * 2**bdilution
+            B0 = 2e-3 * 2**bdilution
             sys.argv[B0index] = str(B0)
 
             run = popdyn.main()
             popsize = np.array(run['N0'])
             print('{:.2e} {:.2e} {:.2e}'.format(N0,B0,popsize[-1]))
             
-            plate[bdilution,ndilution] = popsize[-1]
+            plate[ndilution,bdilution] = popsize[-1]
 
-            prc.PlateImage(data = plate)
+    threshold = computeThreshold(plate)
+    prc.PlateImage(data = plate, growththreshold = threshold)
 
 
 
